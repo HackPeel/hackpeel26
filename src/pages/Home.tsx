38e12c493492402df3ad22867b4978be55f2f718
harjapssingh <161743418+harjapssingh@users.noreply.github.com>
2026-06-03 08:52:40 -0400

    added pcbway

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,7 +19,7 @@ const Home = () => {
 
   // Countdown to a sample event date (adjust this!)
   useEffect(() => {
-    const eventDate = new Date("2026-12-12T09:00:00").getTime();
+    const eventDate = new Date("2026-12-12T08:00:00").getTime();
     
     const timer = setInterval(() => {
       const now = new Date().getTime();
